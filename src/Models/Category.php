2d5234a954bf572120b4cd6fb4bddf09417174b8
Amro Elsabeihi <amrosubihie@gmail.com>
2021-03-09 13:01:32 +0200

    search and sort improvement

--- src/Models/Category.php
+++ src/Models/Category.php
@@ -2,6 +2,7 @@
 
 namespace Sadeem\Commons\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Spatie\Activitylog\Traits\LogsActivity;
 use GoldSpecDigital\LaravelEloquentUUID\Database\Eloquent\Model;
 
@@ -35,34 +36,56 @@ class Category extends Model
   public function searchAndSort($request)
   {
     // Params list
-    $q = $request->only('q');
-
-    $sort = $request->only('sort');
-    $sort_by = $request->only('sort_by');
-
-    if ($sort != 'desc') $sort = 'asc';
-    if (!$sort_by) $sort_by = 'name';
-
-    $filter = $request->only('filter');
-
-    // Conditions list
-    $paramQ = $q;
-    $paramSortBy = confirmColumn($sort_by, config('sadeem.table_names.categories'));
+    $q = $request->input('q', '');
+    $sorts = explode(',', $request->input('sort', ''));
 
     return $this
-      ->when($paramQ, function () use ($sort_by, $q) {
-        return $this->similarity($sort_by, $q);
+      ->when(!empty($q), function () use ($q) {
+        return $this->similarity('name', $q);
       })
-      ->when(!$paramQ && $paramSortBy, function () use ($sort_by, $sort) {
-        return $this->orderBy($sort_by, $sort);
+      ->when(empty($q) && !empty($sorts[0]), function () use ($sorts) {
+        return $this->orderQuery($sorts);
       })
-      ->when(!$paramQ && !$paramSortBy, function () {
+      ->when(empty($q) && empty($sorts), function () {
         return $this;
       });
   }
 
+  /**
+   * @param $column
+   * @param $q
+   * @return mixed
+   */
   public function similarity($column, $q)
   {
     return similarityByColumn($this, $column, $q);
+  }
+
+  /**
+   * Builds a query using multiple sort param values
+   * as in ?sort=parent_id, -is_disabled
+   *
+   * @param $sorts
+   * @return Builder
+   */
+  public function orderQuery($sorts): Builder
+  {
+    $query = $this::query();
+
+    foreach ($sorts as $sortColumn) {
+
+      // remove empty spaces
+      $sortColumn = trim($sortColumn);
+
+      // decide the order direction
+      $sortDirection = str_starts_with($sortColumn, '-') ? 'desc' : 'asc';
+
+      // trim the order direction
+      $sortColumn = ltrim($sortColumn, '-');
+
+      $query->orderBy($sortColumn, $sortDirection);
+    }
+
+    return $query;
   }
 }
