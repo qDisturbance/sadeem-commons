ee1c88b820f35492501d909866fde79ac2b30edc
Amro Elsabeihi <amrosubihie@gmail.com>
2021-03-11 08:39:42 +0200

    applies unique category create/update

--- src/Http/Requests/CategoryRequest.php
+++ src/Http/Requests/CategoryRequest.php
@@ -3,6 +3,8 @@
 namespace Sadeem\Commons\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+use Sadeem\Commons\Models\Category;
 
 class CategoryRequest extends FormRequest
 {
@@ -28,18 +30,41 @@ class CategoryRequest extends FormRequest
 
     $method = $this->method();
 
+    $name = $this->input('name');
+    $parent_id = $this->input('parent_id');
+
     if ($method == "POST") {
       return [
-        'name' => 'required|min:3|max:255',
+        'name' => [
+          'required',
+          'min:2',
+          'max:255',
+          Rule::unique($table)->where(function ($query) use ($name, $parent_id) {
+            return $query
+              ->where('name', $name)
+              ->where('parent_id', $parent_id);
+          }),
+        ],
         'parent_id' => "min:3|max:255|exists:{$table}",
       ];
     };
     if ($method == "PATCH") {
 
       $id = $this->route($model)->id;
+      if (empty($parent_id)) $parent_id = Category::where('id', $id)->get()->pluck('parent_id');
+
       return [
-        'name' => "min:3|max:255",
-        'parent_id' => "min:3|max:255|exists:{$table},parent_id,{$id}",
+        'name' => [
+          'min:2',
+          'max:255',
+          Rule::unique($table)->where(function ($query) use ($id, $name, $parent_id) {
+            return $query
+              ->where('name', $name)
+              ->where('parent_id', $parent_id)
+              ->whereNotIn('id', [$id]);
+          }),
+        ],
+        'parent_id' => "min:3|max:255|exists:{$table},parent_id,{$parent_id}",
         'is_disabled' => 'boolean'
       ];
     };
