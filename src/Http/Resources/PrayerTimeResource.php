6ab27ceea113695342baa4cf3a5847dceef08481
Amro Elsabeihi <amrosubihie@gmail.com>
2021-04-07 21:22:21 +0200

    adds hijri date to prayer time resource

--- src/Http/Resources/PrayerTimeResource.php
+++ src/Http/Resources/PrayerTimeResource.php
@@ -3,7 +3,10 @@
 namespace Sadeem\Commons\Http\Resources;
 
 use Illuminate\Http\Request;
+use Alkoumi\LaravelHijriDate\Hijri;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Carbon;
+
 
 class PrayerTimeResource extends JsonResource
 {
@@ -15,13 +18,27 @@ class PrayerTimeResource extends JsonResource
      */
     public function toArray($request)
     {
+      Hijri::setLang('ar');
+
       $cityCondition = getIncludeCondition(
         $request->input('include'),
         'city'
       );
 
+      $year = Carbon::now()->year;
+
+      $arr = explode('/', $this->day);
+      $day = $arr[0];
+      $month = $arr[1];
+      $date = "{$year}/{$month}/{$day}";
+
+      $hijriDate = Hijri::ShortDate($date);
+      $fullHijriDate = Hijri::Date('l ، j F ، Y', $date);
+
       return [
-        "day" => $this->day,
+        "hijri_long" => $fullHijriDate,
+        "hijri_short" => $hijriDate,
+        "date" => $date,
         "emsak" => $this->emsak,
         "fajer" => $this->fajer,
         "sherook" => $this->sherook,
